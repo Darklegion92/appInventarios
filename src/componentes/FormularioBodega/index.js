86dfeef9b88c15bd39e7512fbcc8f7a719f707ba
JOSE LUIS RODRIGUEZ PEÑARANDA <joserodriguez@saludfamiliar.com.co>
2020-11-20 16:52:27 -0500

    terminado

--- src/componentes/FormularioBodega/index.js
+++ src/componentes/FormularioBodega/index.js
@@ -1,25 +1,34 @@
-import React from 'react'
-import { Input } from 'antd'
+import React from "react";
+import { Input, Select } from "antd";
 
-import './styles.css'
+import "./styles.css";
 
-const FormularioBodegas = props => {
-  const { nombre, setNombre } = props
+const { Option } = Select;
 
-  const onChangeNombre = e => {
-    const { value } = e.target
-    setNombre(value)
-  }
+const FormularioBodegas = (props) => {
+  const { setDatos, datos, sucursales } = props;
+
+  const onChangeNombre = (e) => {
+    const { value } = e.target;
+    //setDatos({id});
+  };
+  console.log(datos);
   return (
-    <div className='formulario-grupos'>
+    <div className="formulario-grupos">
       <Input
-        value={nombre}
-        placeholder='Nombre Bodega'
+        value={datos.nombre}
+        placeholder="Nombre Bodega"
         maxLength={20}
         onChange={onChangeNombre}
       />
+      <Select defaultValue={datos.nombresucursal} style={{ width: "100%" }}>
+        {sucursales &&
+          sucursales.map((sucursal) => {
+            return <Option key={sucursal.id}>{sucursal.nombre}</Option>;
+          })}
+      </Select>
     </div>
-  )
-}
+  );
+};
 
-export default FormularioBodegas
+export default FormularioBodegas;
--- src/componentes/ParametrosInventario/index.js
+++ src/componentes/ParametrosInventario/index.js
@@ -1,100 +1,105 @@
-import React, { useState, useEffect } from 'react'
-import { Typography } from 'antd'
-import axios from 'axios'
-import TablaParametros from '../Tabla'
-import FormularioBodegas from '../FormularioBodega'
-import { API } from '../../config/keys'
+import React, { useState, useEffect, useContext } from "react";
+import { Typography } from "antd";
+import axios from "axios";
+import TablaParametros from "../Tabla";
+import FormularioBodegas from "../FormularioBodega";
+import { GlobalContext } from "../../context/GlobalContext";
+import { API } from "../../config/keys";
 
-import './styles.css'
-const { Title } = Typography
+import "./styles.css";
+const { Title } = Typography;
 
-export default function Parametros () {
-  const [datosBodega, setDatosBodega] = useState()
-  const [idBodega, setIdBodega] = useState(0)
-  const [nombreBodega, setNombreBodega] = useState()
-
-  const okBodega = async editar => {
+export default function Parametros() {
+  const { sucursales } = useContext(GlobalContext);
+  const [datosBodega, setDatosBodega] = useState();
+  const [bodega, setBodega] = useState({});
+  console.log(sucursales);
+  const okBodega = async (editar) => {
     try {
       if (editar) {
         const res = await axios.put(
-          API + 'parametros/bodegas',
+          API + "parametros/bodegas",
           {
-            id: idBodega,
-            nombre: nombreBodega
+            id: bodega.id,
+            nombre: bodega.nombre,
           },
           {
             headers: {
-              authorization: sessionStorage.getItem('Token')
-            }
+              authorization: sessionStorage.getItem("Token"),
+            },
           }
-        )
+        );
         if (res.status === 200) {
-          setDatosBodega(res.data)
+          setDatosBodega(res.data);
         }
       } else {
         const res = await axios.post(
-          API + 'parametros/bodegas',
+          API + "parametros/bodegas",
           {
-            nombre: nombreBodega
+            nombre: bodega.nombre,
           },
           {
             headers: {
-              authorization: sessionStorage.getItem('Token')
-            }
+              authorization: sessionStorage.getItem("Token"),
+            },
           }
-        )
+        );
         if (res.status === 200) {
-          setDatosBodega(res.data)
+          setDatosBodega(res.data);
         }
       }
     } catch (e) {
-      console.log(e)
+      console.log(e);
     }
-  }
+  };
 
-  const cargarBodega = record => {
-    setIdBodega(record.id)
-    setNombreBodega(record.nombre)
-  }
+  const cargarBodega = (record) => {
+    setBodega(record);
+  };
 
   const cargarDatos = async () => {
     try {
-      const grupos = await axios.get(API + 'parametros/bodegas', {
+      const grupos = await axios.get(API + "parametros/bodegas", {
         headers: {
-          authorization: sessionStorage.getItem('Token')
-        }
-      })
+          authorization: sessionStorage.getItem("Token"),
+        },
+      });
       if (grupos.status === 200) {
-        setDatosBodega(grupos.data)
+        setDatosBodega(grupos.data);
       }
     } catch (e) {
-      console.log(e)
+      console.log(e);
     }
-  }
+  };
 
   useEffect(() => {
-    cargarDatos()
-  }, [])
+    cargarDatos();
+  }, []);
 
   const collistabodegas = [
     {
-      title: 'Código',
-      dataIndex: 'id',
-      key: 'id'
+      title: "Código",
+      dataIndex: "id",
+      key: "id",
     },
     {
-      title: 'Nombre',
-      dataIndex: 'nombre',
-      key: 'nombre'
-    }
-  ]
+      title: "Nombre",
+      dataIndex: "nombre",
+      key: "nombre",
+    },
+    {
+      title: "Sucursal",
+      dataIndex: "nombresucursal",
+      key: "nombresucursal",
+    },
+  ];
 
   return (
-    <div className='agrupaciones'>
+    <div className="agrupaciones">
       <Title>AGRUPACIONES ARTÍCULOS</Title>
-      <div className='dos'>
+      <div className="dos">
         <TablaParametros
-          titulo='BODEGAS'
+          titulo="BODEGAS"
           datos={datosBodega}
           columnas={collistabodegas}
           paginacion={5}
@@ -102,12 +107,13 @@ export default function Parametros () {
           edicion={cargarBodega}
           Componente={
             <FormularioBodegas
-              nombre={nombreBodega}
-              setNombre={setNombreBodega}
+              datos={bodega}
+              setDatos={setBodega}
+              sucursales={sucursales}
             />
           }
         />
       </div>
     </div>
-  )
+  );
 }
